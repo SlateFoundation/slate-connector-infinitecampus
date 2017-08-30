d8debd922f3bbe5901a66570425bd9a4f6677a74
Chris Alfano <chris@jarv.us>
2017-08-30 11:36:12 -0400

    Add git configuration

---
+++ php-config/Git.config.d/slate-connector-infinitecampus.php
@@ -0,0 +1,14 @@
+<?php
+
+Git::$repositories['slate-connector-infinitecampus'] = [
+    'remote' => 'https://github.com/SlateFoundation/slate-connector-infinitecampus.git',
+    'originBranch' => 'master',
+    'workingBranch' => 'master',
+    'trees' => [
+        'html-templates/connectors/infinite-campus/createJob.tpl',
+        'php-classes/Slate/Connectors/AbstractSpreadsheetConnector.php', // TODO: merge upstream
+        'php-classes/Slate/Connectors/InfiniteCampus/Connector.php',
+        'php-config/Git.config.d/slate-connector-infinitecampus.php',
+        'site-root/connectors/infinite-campus.php'
+    ]
+];
